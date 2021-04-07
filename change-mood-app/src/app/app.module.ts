eeb656d3e8ecc3978af56aeb29bdfddf123e1584
anna <curly160795@gmail.com>
2021-04-07 13:19:12 +0300

    added details/error/login/signup components and services

--- change-mood-app/src/app/app.module.ts
+++ change-mood-app/src/app/app.module.ts
@@ -3,16 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { WeatherCardComponent } from './ui/weather-card/weather-card.component';
+import { AddCardComponent } from './ui/add-card/add-card.component';
+import {WeatherService} from "./services/weather/weather.service";
+import {DetailsComponent} from "./pages/details-component/details.component";
+import { AddComponent } from './pages/add/add.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { ErrorComponent } from './ui/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WeatherCardComponent,
+    AddCardComponent,
+    DetailsComponent,
+    AddComponent,
+    LoginComponent,
+    SignupComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
