01bc009c92eafe15847573c2e375d2570793d2d0
anna <curly160795@gmail.com>
2021-04-09 15:13:23 +0300

    added guards

--- change-mood-app/src/app/app-routing.module.ts
+++ change-mood-app/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import {DetailsComponent} from "./pages/details-component/details.component";
 import {AddComponent} from "./pages/add/add.component";
 import {LoginComponent} from "./pages/login/login.component";
 import {SignupComponent} from "./pages/signup/signup.component";
+import {AppGuard} from "./guards/app.guard";
+import {AuthGuard} from "./guards/auth.guard";
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'details/:city', component: DetailsComponent},
-  {path: 'add', component: AddComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'signup', component: SignupComponent},
+  {path: '', component: HomeComponent, canActivate: [AppGuard]},
+  {path: 'details/:city', component: DetailsComponent, canActivate: [AppGuard]},
+  {path: 'add', component: AddComponent, canActivate: [AppGuard]},
+  {path: 'login', component: LoginComponent, canActivate: [AuthGuard]},
+  {path: 'signup', component: SignupComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
